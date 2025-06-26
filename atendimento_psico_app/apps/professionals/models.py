8932dd62dfd70e0c4a5d1c9633b3507c74019efb
Enzo <davila.commercial@gmail.com>
2025-06-25 21:50:45 -0300

    modelar classe

---
+++ atendimento_psico_app/apps/professionals/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Especialidade(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Profissional(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+    ]
+    name = models.CharField(max_length=100)
+    cell_phone = models.CharField(max_length=15)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class ProfissionalEspecialidade(models.Model):
+    profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE)
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.profissional.name} - {self.especialidade.name}"
