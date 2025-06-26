72c203c645418b6f8415a38230e4d53f57de5e75
Enzo <davila.commercial@gmail.com>
2025-06-26 00:45:04 -0300

    implementar profissionais

--- atendimento_psico_app/atendimento_psico_app/settings.py
+++ atendimento_psico_app/atendimento_psico_app/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'categories.apps.CategoriesConfig',
-    'products.apps.ProductsConfig',
+    'apps.profissionais',
+    
 ]
 
 MIDDLEWARE = [
