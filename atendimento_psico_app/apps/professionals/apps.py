df3af0e188ad85586670c3895bbf14c1fb080dac
Enzo <davila.commercial@gmail.com>
2025-06-25 22:49:50 -0300

    adicionar view prof

--- atendimento_psico_app/apps/professionals/apps.py
+++ atendimento_psico_app/apps/professionals/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ProfessionalsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'professionals'
+    verbose_name = 'Profissionais'
--- atendimento_psico_app/apps/professionals/views.py
+++ atendimento_psico_app/apps/professionals/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Especialidade, Profissional, ProfissionalEspecialidade
+from .serializer import EspecialidadeSerializer, ProfissionalSerializer, ProfissionalEspecialidadeSerializer
 # Create your views here.
+
+
+class EspecialidadeViewSet(viewsets.ModelViewSet):
+    queryset = Especialidade.objects.all()
+    serializer_class = EspecialidadeSerializer
+
+class ProfissionalViewSet(viewsets.ModelViewSet):
+    queryset = Profissional.objects.all()
+    serializer_class = ProfissionalSerializer
+
+class ProfissionalEspecialidadeViewSet(viewsets.ModelViewSet):
+    queryset = ProfissionalEspecialidade.objects.all()
+    serializer_class = ProfissionalEspecialidadeSerializer
